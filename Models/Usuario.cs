c21a020f7f0897f37d5953ae23276730463906bf
Oscar Cabagné <OCabagne@outlook.com>
2022-08-25 19:42:43 -0500

    Binding to Profile Page

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Escuela { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
--- Pages/ProfilePage.xaml.cs
+++ Pages/ProfilePage.xaml.cs
@@ -1,11 +1,24 @@
+using test1.Models;
+
 namespace test1.Pages;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
+	public Usuario User { get; private set; } 
+    public ProfilePage()
 	{
 		InitializeComponent();
-	}
+
+		User = new Usuario 
+		{ 
+			Id = 1, 
+			Nombre = "Oscar Cabagné", 
+			Escuela = "ESCOM",
+			Url = "https://scontent.fmex31-1.fna.fbcdn.net/v/t39.30808-6/259971623_6626299194110531_4205802413890419082_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NSbgGJJRoQgAX8TsReT&_nc_ht=scontent.fmex31-1.fna&oh=00_AT-mb9xItwpDpClS9FBqfOsF2355YH-X2Uvu9rDsCT1lDw&oe=630DB638"
+        };
+
+        this.BindingContext = User;
+    }
 
 	private void FavoritosBtn_Clicked(object sender, EventArgs e)
 	{
